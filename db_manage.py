b56b290626d60e8bc236fcc6ba4c4e9e283f4070
kmg0157 <kmg0157@naver.com>
2024-06-23 15:46:18 +0200

    240623_1046

--- db_manage.py
+++ db_manage.py
@@ -3,22 +3,24 @@ import base64
 from datetime import datetime
 from PIL import Image
 import io
+from client import FileReceiver
 
 class ImageDatabase:
-    def __init__(self, db_path='image_database.db'):
-        # 데이터베이스 연결 (데이터베이스 파일이 없으면 자동으로 생성됨)
-        self.conn = sqlite3.connect(db_path, check_same_thread=False)
+    def __init__(self):
+        # 데이터베이스 연결
+        self.conn = sqlite3.connect('image_database.db', check_same_thread=False)
         # 커서 객체 생성
         self.cur = self.conn.cursor()
         self.create_table()
+        self.image_data=FileReceiver()
 
     def create_table(self):
         # 테이블 생성 SQL
         create_table_query = '''
         CREATE TABLE IF NOT EXISTS images (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
             timestamp INTEGER NOT NULL,
             dimensions TEXT NOT NULL,
+            filename TEXT NOT NULL,
             image_blob BLOB NOT NULL,
             status INTEGER NOT NULL
         )
@@ -27,6 +29,7 @@ class ImageDatabase:
         self.cur.execute(create_table_query)
         self.conn.commit()
 
+    #수정필요
     def save_image(self, image_bytes, status):
         # 이미지 바이트로부터 이미지 객체 생성
         img = Image.open(io.BytesIO(image_bytes))
@@ -50,36 +53,6 @@ class ImageDatabase:
         self.cur.execute(insert_data_query, (timestamp, dimensions, image_base64, status))
         self.conn.commit()
 
-    def update_image_status(self, image_id, status):
-        # 이미지 상태 업데이트 SQL
-        update_status_query = '''
-        UPDATE images
-        SET status = ?
-        WHERE id = ?
-        '''
-        # 상태 업데이트
-        self.cur.execute(update_status_query, (status, image_id))
-        self.conn.commit()
-
-    def fetch_images(self):
-        # 이미지 데이터 가져오기 SQL
-        fetch_images_query = '''
-        SELECT id, timestamp, dimensions, image_blob, status
-        FROM images
-        '''
-        self.cur.execute(fetch_images_query)
-        rows = self.cur.fetchall()
-        images = []
-        for row in rows:
-            image = {
-                'id': row[0],
-                'timestamp': row[1],
-                'dimensions': row[2],
-                'image_blob': row[3],
-                'status': row[4]
-            }
-            images.append(image)
-        return images
 
     def close_db(self):
         # 커서와 연결 종료
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from client import FileReceiver
+from db_manage import ImageDatabase
 
 def main():
     client = FileReceiver()
+    imagedb=ImageDatabase()
     try:
         client.run()
         client._set_routes()
+        imagedb.save_image()
+        imagedb.close_db()
     except KeyboardInterrupt:
         pass
 
